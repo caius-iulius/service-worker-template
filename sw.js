9803c1d499ba3fe4c047377afc51f664224a6194
Caius Iulius Caesar <51883767+caiusiuliuscaesar-sites@users.noreply.github.com>
2019-06-16 18:46:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,7 +9,7 @@ self.addEventListener('install', function(event) {
         '/assets/js/sidenav.js',
         '/assets/placeholderpage.html'
       ]);
-    })
+    }.catch(function(err){console.log(err);}))
   );
 });
 
