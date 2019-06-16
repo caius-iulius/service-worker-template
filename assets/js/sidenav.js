b02bacd97467285de7f375bb74fec01fa009e6bc
Caius Iulius Caesar <51883767+caiusiuliuscaesar-sites@users.noreply.github.com>
2019-06-16 18:29:20 +0200

    Add files via upload

---
+++ assets/js/sidenav.js
@@ -0,0 +1,2 @@
+function openNav(){document.getElementById("openNav").style.visibility="hidden";document.getElementById("mySidenav").style.width="250px"}
+function closeNav(){document.getElementById("mySidenav").style.width="0";document.getElementById("openNav").style.visibility="visible"}
