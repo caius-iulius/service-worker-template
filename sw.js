54b16c845592b84646a8d0a421afccc1af4dbcff
Caius Iulius Caesar <51883767+caiusiuliuscaesar-sites@users.noreply.github.com>
2019-06-16 22:04:09 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,30 +7,36 @@ var toCache = [
         '/assets/placeholderpage.html'
 ];
 var cacheName = 'cic-cache-v1';
+var swVersion = 'v1';
 
 self.addEventListener('install', function(event) {
   event.waitUntil(
     caches.open(cacheName).then(function(cache) {
       return cache.addAll(toCache);
-    }).catch(function(err){console.log("Error at sw-install: "+err);})
+    }).then(function(){console.log("Service Worker "+swVersion+" installed!")}).catch(function(err){console.log("Error at sw-install: "+err);})
   );
 });
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
       return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
         let responseClone = response.clone();
+        
         caches.open(cacheName).then(function (cache) {
           cache.put(event.request, responseClone);
         });
-        
         return response;
       }).catch(function () {
-        if (response !== undefined) {
-          return response;
-        } else {
-          return caches.match('/assets/placeholderpage.html');
-        }
+        return caches.match('/assets/placeholderpage.html');
       });
+    }
   }));
 });
