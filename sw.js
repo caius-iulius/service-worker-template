50907faa35a274bb685254b00ba6cca59062f222
Caius Iulius Caesar <51883767+caiusiuliuscaesar-sites@users.noreply.github.com>
2019-06-16 19:16:02 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -13,6 +13,22 @@ self.addEventListener('install', function(event) {
   );
 });
 
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.open('cic-cache-v1').then(function(cache) {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/assets/css/styles.min.css',
+        '/app.js',
+        '/assets/js/sidenav.js',
+        '/assets/placeholderpage.html'
+      ]);
+    }).catch(function(err){console.log("Error at sw-install: "+err);})
+  );
+});
+
+
 self.addEventListener('fetch', function(event) {
   event.respondWith(caches.match(event.request).then(function(response) {
     // caches.match() always resolves
