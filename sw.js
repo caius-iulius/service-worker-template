a0a6292e220635830c4e67d5171a184141ff4ce1
Caius Iulius Caesar <51883767+caiusiuliuscaesar-sites@users.noreply.github.com>
2019-06-16 22:25:19 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,22 +9,26 @@ var toCache = [
 var cacheName = 'cic-cache-v1';
 var swVersion = 'v0.1.1';
 
-self.addEventListener('fetch', function(event) { //network first
+self.addEventListener('fetch', function(event) { //cache first
   event.respondWith(caches.match(event.request).then(function(response) {
+    // caches.match() always resolves
+    // but in case of success response will have value
+    if (response !== undefined) {
+      return response;
+    } else {
       return fetch(event.request).then(function (response) {
+        // response may be used only once
+        // we need to save clone to put one copy in cache
+        // and serve second one
         let responseClone = response.clone();
-        caches.open(cacheName).then(function (cache) {
+        
+        caches.open('cic-cache-v1').then(function (cache) {
           cache.put(event.request, responseClone);
         });
-        
         return response;
       }).catch(function () {
-        if (response !== undefined) {
-          return response;
-        } else {
-          return caches.match('/assets/placeholderpage.html');
-        }
+        return caches.match('/assets/placeholderpage.html');
       });
+    }
   }));
 });
-
